a58162bc63157c48f1cfefb106919345d5b8967a
yusptar <yusupvatar@gmail.com>
2022-01-05 14:07:31 +0700

    add profile progress feature

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('layouts.global');
 });
+
+Route::get('profile', 'App\Http\Controllers\FrontController@profile');
