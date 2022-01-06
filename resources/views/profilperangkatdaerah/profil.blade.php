82cb011efe2fde19f30c377404acdd04de64adba
yusptar <yusupvatar@gmail.com>
2022-01-06 14:36:24 +0700

    add save alert for profile page

--- resources/views/profilperangkatdaerah/profil.blade.php
+++ resources/views/profilperangkatdaerah/profil.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.global')
 @section('title', 'Profil')
 @section('container')
-   <h1 class="h3 col-md-6 row-md-8 mb-3 justify-content-around mb-0 text-gray-800 font-weight-bold">Profile Perangkat Daerah</h1>
+   <h1 class="h3 col-md-6 row-md-8 mb-3 justify-content-around mb-0 text-gray-800 font-weight-bold">Profil Perangkat Daerah</h1>
    <div class="col-lg-12">
    <form method="post" action="#" class="mb-5" enctype="multipart/form-data">
    @csrf
@@ -63,10 +63,30 @@
    <br>
    <div class="ml-4 row">
       <div class="col-sm-8">
-         <button type="button" class="btn btn-primary btn-lg float-right btn-sm">Simpan</button>
+         <button type="button" class="btn btn-primary btn-lg float-right btn-sm" data-toggle="modal" data-target="#exampleModalCenter">Simpan</button>
       </div>
    </div>
 </form>
+</div>
+
+<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <!-- <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLongTitle"></h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div> -->
+      <div class="modal-body">
+        Apakah anda yaking ingin Submit?<br>Mohon periksa kembali isian anda
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Batal</button>
+        <button type="button" class="btn btn-primary">Simpan</button>
+      </div>
+    </div>
+  </div>
 </div>
 @endsection
 
