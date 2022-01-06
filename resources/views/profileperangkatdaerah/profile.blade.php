804620dd95b5f56ce529bd2a31cf9784ae5575d7
yusptar <yusupvatar@gmail.com>
2022-01-06 11:35:15 +0700

    update profile session

--- resources/views/profileperangkatdaerah/profile.blade.php
+++ resources/views/profileperangkatdaerah/profile.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.global')
 @section('title', 'Profil')
 @section('container')
-   <h1>Profile</h1>
+   <h1 class="h3 col-md-6 row-md-8 mb-3 justify-content-around mb-0 text-gray-800 font-weight-bold">Profile Perangkat Daerah</h1>
 @endsection
 
