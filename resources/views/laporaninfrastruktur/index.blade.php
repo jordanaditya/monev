564f28488e1ac68a4dee842f185bfc52f9faca4a
Firmanrachmad <dragobniel@gmail.com>
2022-01-06 11:24:15 +0700

    update icon

--- resources/views/laporaninfrastruktur/index.blade.php
+++ resources/views/laporaninfrastruktur/index.blade.php
@@ -1,5 +1,30 @@
 @extends('layouts.global')
 @section('title', 'Laporan Infrastruktur TIK')
 @section('container')
-<h1 class="h3 col-md-6 row-md-8 mb-3 justify-content-around mb-0 text-gray-800 font-weight-bold">Monev Infrastruktur TIK</h1>
+<div class="row-3">
+    <h1 class="h3 col-md-6 row-md-8 mb-3 justify-content-around mb-0 text-gray-800 font-weight-bold">Monev Infrastruktur TIK</h1>
+</div>
+
+<div class="container">
+    <br><br>
+
+    <div class="row">
+        <div class="col">
+            <img src="{{ asset('img/user.png') }}" width="85px" height="85px">
+        </div>
+        <div class="col">
+            <img src="{{ asset('img/document.png') }}" width="85px" height="85px">
+        </div>
+        <div class="col">
+            <img src="{{ asset('img/computer.png') }}" width="85px" height="85px">
+        </div>
+        <div class="col">
+            <img src="{{ asset('img/dna.png') }}" width="85px" height="85px">
+        </div> 
+    </div> 
+</div>
+
+
+
+
 @endsection
